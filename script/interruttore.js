0d76e2a0c956da271f0c9636bd947eaf600e76eb
davide <serri98@gmail.com>
2016-05-24 13:12:33 +0200

    aggiunta cartella script + divisione js

--- script/interruttore.js
+++ script/interruttore.js
@@ -1,14 +1,3 @@
-
-//COLLEGAMENTO FIREBASE
-var config = {
-    apiKey: "AIzaSyCdegHgPn2Uklg8DuDRt1JPP1rvdvzy3sM",
-    authDomain: "aromatic-5cf2a.firebaseapp.com",
-    databaseURL: "https://aromatic-5cf2a.firebaseio.com",
-    storageBucket: "aromatic-5cf2a.appspot.com",
-};
-
-firebase.initializeApp(config);
-//FINE COLLEGAMENTO AL DATABASE
 //Controllo interruttore luce
 var valoreInterruttore;
 
@@ -71,6 +60,5 @@ function accensione() {
        firebase.database().ref("luci").update(aggiornamenti);
         outputAccensione();
     }
-
-
+	
 }

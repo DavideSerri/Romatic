0551a46d5a77d6cbaff12eb687c18d0a2a39902f
yuri.gardinazzi <gardinazzi.yuri@gmail.com>
2016-05-24 11:15:46 +0200

    Aggiunta collegamento a firebase

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
+
+//COLLEGAMENTO FIREBASE
+var config = {
+    apiKey: "AIzaSyCrShxGQFW1DoVeupCCLvOoEA10a7jJ-9s",
+    authDomain: "blinding-inferno-1936.firebaseapp.com",
+    databaseURL: "https://blinding-inferno-1936.firebaseio.com",
+    storageBucket: "blinding-inferno-1936.appspot.com",
+};
+firebase.initializeApp(config);
 var interruttore = document.getElementById("interruttore");
     interruttore.addEventListener("click", accensione, false);
 

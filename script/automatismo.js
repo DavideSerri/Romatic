a5d745c084341092b4c2b6281d6a1c4983737457
davide <serri98@gmail.com>
2016-05-27 11:25:04 +0200

    aggiunto disabilita bottone in automatico

--- script/automatismo.js
+++ script/automatismo.js
@@ -1,12 +1,19 @@
 function outputAutomatismi() {
     var bottoneLuce = document.getElementById("luciAutomatiche");
     var bottonePompa = document.getElementById("pompaAutomatica");
+	var bottoneLuce2 = document.getElementById("interruttore");
+	var bottonePompa2 = document.getElementById("motore");
     firebase.database().ref("luci").once("value").then(function (snapshot) {
        
         if (snapshot.val().automatico) {
             bottoneLuce.checked=true;
+			//bottoneLuce2.setAttribute("disabled", "true");
+			bottoneLuce2.disabled = true;
+			
         } else {
             bottoneLuce.checked=false;
+			//bottoneLuce2.setAttribute("disabled", "false");
+			bottoneLuce2.disabled = false;
         }
     });
     firebase.database().ref("serbatoio").once("value").then(function (snapshot) {
@@ -14,8 +21,12 @@ function outputAutomatismi() {
         if (snapshot.val().automatico) {
             
             bottonePompa.checked = true;
+			//bottonePompa2.setAttribute("disabled", "true");
+			bottonePompa2.disabled = true;
         } else {
             bottonePompa.checked = false;
+			//bottonePompa2.setAttribute("disabled", "false");
+			bottonePompa2.disabled = false;
         }
     });
 }
@@ -30,23 +41,33 @@ function cambioAutomaticoLuce() {
         
         cambio["automatico"] = false;
         firebase.database().ref("luci").update(cambio);
+		//bottoneLuce2.setAttribute("disabled", "false");
+		bottoneLuce2.disabled = false;
     } else {       
         cambio["automatico"] = true;    
         firebase.database().ref("luci").update(cambio);
+		//bottoneLuce2.setAttribute("disabled", "true");
+		bottoneLuce2.disabled = true;
     } 
     outputAutomatismi();
     });
 }
 function cambioAutomaticoPompa() {
+	var bottoneLuce2 = document.getElementById("interruttore");
+	var bottonePompa2 = document.getElementById("motore");
     cambio = {};
     firebase.database().ref("serbatoio").once("value").then(function (snapshot) {
         if (snapshot.val().automatico) {
 
             cambio["automatico"] = false;
             firebase.database().ref("serbatoio").update(cambio);
+			//bottonePompa2.setAttribute("disabled", "false");
+			bottonePompa2.disabled = false;
         } else {
             cambio["automatico"] = true;
             firebase.database().ref("serbatoio").update(cambio);
+			//bottonePompa2.setAttribute("disabled", "true");
+			bottonePompa2.disabled = true;
         }
         outputAutomatismi();
     });
--- script/sensoreLivello.js
+++ script/sensoreLivello.js
@@ -2,6 +2,19 @@
 function outputLivelloAcqua() {
     var outputLivello = document.getElementById("sensoreLivello");
     firebase.database().ref("serbatoio").once("value").then(function (snapshot) {
-        outputLivello.innerHTML = "Livello Acqua: " + snapshot.val().sensoreLivello;
+		if (snapshot.val().sensoreLivello <= 10) {
+			
+		outputLivello.innerHTML = "<span class='glyphicon glyphicon-warning-sign' aria-hidden='true'></span>"+" Ricarica Serbatoio!";	
+		outputLivello.style.width = "100%";
+		outputLivello.className = "progress-bar progress-bar-danger";		
+		}
+		else {
+		outputLivello.className = "progress-bar progress-bar-info";	
+        outputLivello.innerHTML = snapshot.val().sensoreLivello+"%";
+		outputLivello.style.width = snapshot.val().sensoreLivello+"%";
+		}
     });
+	
+	
 }
+

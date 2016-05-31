a677cae074831a084b14c61921a7cd73601dea5f
davide <serri98@gmail.com>
2016-05-31 11:14:39 +0200

    prima versione grafica login

---
+++ script/Login.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCdegHgPn2Uklg8DuDRt1JPP1rvdvzy3sM",
+    authDomain: "aromatic-5cf2a.firebaseapp.com",
+    databaseURL: "https://aromatic-5cf2a.firebaseio.com",
+    storageBucket: "aromatic-5cf2a.appspot.com",
+};
+
+firebase.initializeApp(config);

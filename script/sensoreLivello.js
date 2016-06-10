1d4c2d4776208fb208f8251436a6d98c84a4f73c
davide <serri98@gmail.com>
2016-06-10 17:13:53 +0200

    sistemato livello serbatoio

--- script/sensoreLivello.js
+++ script/sensoreLivello.js
@@ -2,16 +2,23 @@
 function outputLivelloAcqua() {
     var outputLivello = document.getElementById("sensoreLivello");
     firebase.database().ref("/raspberry/\"" + numeroPrototipo + "\"/serbatoio").once("value").then(function (snapshot) {
-		if (snapshot.val().sensoreLivello <= 10) {
+		if (snapshot.val().sensoreLivello <= 150) {
 			
-		outputLivello.innerHTML = "<span class='glyphicon glyphicon-warning-sign' aria-hidden='true'></span>"+" Ricarica Serbatoio!";	
+		outputLivello.innerHTML = "<span class='glyphicon glyphicon-warning-sign' aria-hidden='true'></span>"+" Motore Spento! Ricarica Serbatoio!";	
 		outputLivello.style.width = "100%";
 		outputLivello.className = "progress-bar progress-bar-danger";		
 		}
 		else {
-		outputLivello.className = "progress-bar progress-bar-info";	
-        outputLivello.innerHTML = snapshot.val().sensoreLivello+"%";
-		outputLivello.style.width = snapshot.val().sensoreLivello+"%";
+			if (snapshot.val().sensoreLivello >= 450) {	
+			outputLivello.className = "progress-bar progress-bar-success";	
+			outputLivello.innerHTML = "Livello acqua normale";
+			outputLivello.style.width = "100%";
+			}
+			else {
+				outputLivello.className = "progress-bar progress-bar-warning";	
+				outputLivello.innerHTML = "<span class='glyphicon glyphicon-warning-sign' aria-hidden='true'></span>"+" Ricarica Serbatoio!";
+				outputLivello.style.width = "50%";				
+			}
 		}
     });
 	
